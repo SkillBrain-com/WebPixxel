833829ca7c4dbbadecebc7bbe79c2da57ecfabf3
mirunalacra <miruna.lacramioara@gmail.com>
2024-07-02 21:20:54 +0300

    modificat

--- src/main/java/common/page/listGroups/ListGroupsPage.java
+++ src/main/java/common/page/listGroups/ListGroupsPage.java
@@ -1,4 +1,4 @@
-package common.listGroups;
+package common.page.listGroups;
 
 import com.sun.tools.javac.util.List;
 import common.base.BaseTest;
@@ -17,4 +17,28 @@ public class ListGroupsPage extends BaseTest{
 
     @FindBy(xpath = "//div[@class='d-flex align-items-center gap-1']//div[@class='d-inline-flex ms-auto gap-3']//div[@class='d-inline-flex gap-1 align-items-center text-body-tertiary']//i[@class='bi bi-heart-fill text-sm text-opacity-50-hover text-opacity-100 text-body-tertiary']")
     private List <WebElement> listGroupsHearts;
+
+    @FindBy(xpath = "//div[@class='dropdown-menu show']//a")
+    private List<WebElement> dropDownLibrary;
+
+    @FindBy(xpath = "//div[@class='d-flex gap-4 align-items-center w-100 undefined']//li")
+    private List<WebElement> listGroupsButton;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
+
+//setTimeout(function(){debugger;},5000); se pune in inspect la console sa inghetam pagina si sa cautam anumite elemente
