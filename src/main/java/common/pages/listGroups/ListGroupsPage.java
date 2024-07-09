341ad452a6860676bac1b5ede74cd38cc79f96fb
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-07-09 19:50:24 +0300

    updated

--- src/main/java/common/pages/listGroups/ListGroupsPage.java
+++ src/main/java/common/pages/listGroups/ListGroupsPage.java
@@ -1,9 +1,5 @@
-<<<<<<<< HEAD:src/main/java/common/pages/listGroups/ListGroupsPage.java
-package common.pages.listGroups;
-========
-package page.page.listGroups;
->>>>>>>> 688055a (adaugare):src/main/java/page/page/listGroups/ListGroupsPage.java
 
+package common.pages.listGroups;
 import com.sun.tools.javac.util.List;
 import common.pages.base.BaseTest;
 import org.openqa.selenium.WebElement;
